0c873a3a342deef8aa2c1faa4496b4f9597f41ac
Daniel <biyuyisdan@gmail.com>
2022-07-16 17:59:53 -0500

    Se creo un script para obtener los autos de la Base de Datos

---
+++ getAutos.php
@@ -0,0 +1,25 @@
+<?php
+
+include 'database.php';
+
+$query = "SELECT * FROM producto";
+$result = mysqli_query($conn, $query);
+
+if(!$result){
+    die("Querie failed!");
+}
+
+while($autos = mysqli_fetch_array($result)){
+    echo "<pre>";
+        echo "\n".$autos['descripcion'];
+        echo "\n".$autos['price'];
+        echo "\n".$autos['dislikes'];
+        echo "\n".$autos['likes'];
+        echo "\n".$autos['loves'];
+        echo "\n".$autos['dir_img'];
+    echo "</pre>";
+    echo  $_SERVER['REQUEST_URI'];
+}
+
+
+?>
